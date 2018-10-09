9b7d0534b47a66c3b5677a2798b0fcc221fba531
Andres Felipe Gomez Barbosa <afgomezb20@gmail.com>
2018-10-09 17:19:26 -0500

    Maven repository updated

--- src/main/java/com/safesociety/fingerprint/services/ClientServicew.java
+++ src/main/java/com/safesociety/fingerprint/services/ClientServicew.java
@@ -15,7 +15,10 @@ import javax.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -49,8 +52,22 @@ public class ClientServicew {
         return client;
     }
     
-    @GetMapping("/client/{id}")
-    public Client getClientById(@PathVariable Integer id) {
+    @RequestMapping(value = "/client/{id}", method = RequestMethod.GET)
+    public Client getClientById(@PathVariable(value = "id") Integer id) {
         return getJpaController().findClient(id);
+    }
+    
+    @RequestMapping(value = "/clients/{id}", method = RequestMethod.PUT)
+    public Client updateClient(@PathVariable(value = "id") Integer id, @Valid @RequestBody Client client) {
+        Client clientAux = getJpaController().findClient(id);
+        clientAux.setAge(client.getAge());
+        clientAux.setLastName(client.getLastName());
+        clientAux.setName(client.getName());
+        try {
+            getJpaController().edit(clientAux);
+        } catch (Exception ex) {
+            Logger.getLogger(ClientServicew.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return clientAux;
     }
 }
